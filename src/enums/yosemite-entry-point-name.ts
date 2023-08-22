090174238102f572616db5ecd1fc9f72a60f286f
abroekhof <abroekhof@google.com>
2023-08-22 06:31:39 -0700

    Add a few more Yosemite trailheads

--- src/enums/yosemite-entry-point-name.ts
+++ src/enums/yosemite-entry-point-name.ts
@@ -4,6 +4,9 @@ enum YosemiteEntryPointName {
 	ALDER_CREEK = 'Alder Creek',
 	HAPPY_ISLES_LITTLE_YOSEMITE_VALLEY = 'Happy Isles - Little Yosemite Valley',
 	HAPPY_ISLES_PAST_LITTLE_YOSEMITE_VALLEY = 'Happy Isles - Past Little Yosemite Valley',
+	RAFFERTY_CREEK = 'Rafferty Creek->Vogelsang',
+	LYELL_NO_DONOHUE = 'Lyell (No Donohue Pass)',
+	LYELL_DONOHUE_ELIGIBLE = 'Lyell Canyon (Donohue Pass Eligible)',
 }
 
 export default YosemiteEntryPointName;
